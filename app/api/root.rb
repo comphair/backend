04efcf1b10deb11c9fc35f40b7d1498f7b2fcfbe
Johannes Schickling <schickling.j@gmail.com>
2013-10-01 00:18:16 +0200

    Renamed api

--- app/api/root.rb
+++ app/api/root.rb
@@ -1,7 +1,7 @@
 module API
   class Root < Grape::API
     mount API::Hairdressers
-    add_swagger_documentation mount_path: "/api",
+    add_swagger_documentation mount_path: "/docs",
                               markdown: true,
                               hide_documentation_path: true
   end
