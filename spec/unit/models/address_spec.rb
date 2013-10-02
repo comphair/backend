d1e4d32226b958bc7257dee8fe4bdef87ad17aee
Johannes Schickling <schickling.j@gmail.com>
2013-10-02 16:21:46 +0200

    Added address test

--- spec/unit/models/address_spec.rb
+++ spec/unit/models/address_spec.rb
@@ -3,9 +3,23 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe Address do
 
   describe "geocoding" do
-    it "should do something" do
-      address = Address.new zipcode: 12093
+
+    it "should adjust coordinates after validation" do
+      Geocoder.configure(:lookup => :test)
+      Geocoder::Lookup::Test.add_stub("96 E 7th St, 10009 New York, United States", [{ 'latitude' => 40.7143528, 'longitude' => -74.0059731 }])
+      address = Address.new({
+        street: "96 E 7th St",
+        zipcode: "10009",
+        city: "New York",
+        country: "United States"
+      })
+      address.latitude.should == 0
+      address.longitude.should == 0
+      address.valid?
+      address.latitude.should == 40.7143528
+      address.longitude.should == -74.0059731
     end
+
   end
 
 end
