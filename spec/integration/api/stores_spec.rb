32a2393a44721ba77f46bf883a302bb1c01fa1d2
Johannes Schickling <schickling.j@gmail.com>
2013-10-04 11:19:09 +0200

    Added stores spec

--- spec/integration/api/stores_spec.rb
+++ spec/integration/api/stores_spec.rb
@@ -5,18 +5,97 @@ describe API::Stores do
   describe "GET /stores" do
 
     before(:each) do
-
+      [{
+        title: "Samar Spa & salon",
+        rating: 5,
+        address: { # coordiantes (40.780056, -73.946570)
+          street: "1758 1st Ave",
+          zipcode: "10128",
+          city: "New York",
+          state: "NY",
+          country: "United States"
+        }
+      }, {
+        title: "Jean Louis David",
+        rating: 4,
+        address: {
+          street: "1180 Avenue of the Americas #2",
+          zipcode: "10036",
+          city: "New York",
+          state: "NY",
+          country: "United States"
+        }
+      }, {
+        title: "Yves Durif Salon",
+        rating: 3,
+        address: {
+          street: "35 E 76th St",
+          zipcode: "10021",
+          city: "New York",
+          state: "NY",
+          country: "United States"
+        }
+      }, {
+        title: "Samar Spa & salon",
+        rating: 2,
+        address: {
+          street: "172 W 85th St",
+          zipcode: "10024",
+          city: "New York",
+          state: "NY",
+          country: "United States"
+        }
+      }].each do |store|
+        store[:address] = FactoryGirl.build(:address, store[:address])
+        FactoryGirl.create(:store, store)
+      end
     end
 
-    it "should return " do
+    it "should return stores near (40.780056, -73.946570)" do
       get "/stores", {
         coordinate: {
           latitude: 40.780056,
           longitude: -73.946570
         }
       }
-      # puts last_response.body
-      # JSON.parse(last_response.body)["customer"].should == { "id" => 1, "name" => "John Doe" }
+      expect(last_response.status).to be 200
+      expect(JSON.parse(last_response.body)).to eq([{
+        "title" => "Samar Spa & salon",
+        "description" => "Description of Samar Spa & salon",
+        "rating" => 5.0,
+        "address" => {
+          "first_name" => "John",
+          "last_name" => "Doe",
+          "street" => "1758 1st Ave",
+          "street_additional" => "96 E 7th St",
+          "zipcode" => "10128",
+          "city" => "New York",
+          "state" => "NY",
+          "country" => "United States",
+          "phone" => "+1 201-369-7000"
+        },
+        "schedule" => {
+          "timeslots" => []
+        }
+      }, {
+        "title" => "Yves Durif Salon",
+        "description" => "Description of Yves Durif Salon",
+        "rating" => 3.0,
+        "address" => {
+          "first_name" => "John",
+          "last_name" => "Doe",
+          "street" => "35 E 76th St",
+          "street_additional" => "96 E 7th St",
+          "zipcode" => "10021",
+          "city" => "New York",
+          "state" => "NY",
+          "country" => "United States",
+          "phone" => "+1 201-369-7000"
+        },
+        "schedule" => {
+          "timeslots" => []
+        }
+      }])
     end
 
   end
