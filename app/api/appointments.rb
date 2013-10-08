f497d6726b3447e206928ec0ac3422d993bffbd5
Johannes Schickling <schickling.j@gmail.com>
2013-10-08 11:44:08 +0200

    Installed stripe

--- app/api/appointments.rb
+++ app/api/appointments.rb
@@ -6,6 +6,27 @@ class API::Appointments < API::Base
     optional :comment, type: String, desc: "Comment"
   end
   post 'appointments' do
+    # TODO transaction
+    if false
+      # Set your secret key: remember to change this to your live secret key in production
+      # See your keys here https://manage.stripe.com/account
+      Stripe.api_key = "sk_test_k5oqJzkJRwMCoOfs392iBqdz"
+
+      # Get the credit card details submitted by the form
+      token = params[:stripeToken]
+
+      # Create the charge on Stripe's servers - this will charge the user's card
+      begin
+        charge = Stripe::Charge.create(
+          :amount => 1000, # amount in cents, again
+          :currency => "usd",
+          :card => token,
+          :description => "payinguser@example.com"
+        )
+      rescue Stripe::CardError => e
+        # The card has been declined
+      end
+    end
     Appointment.create!({
       start_minutes: 1,
       comment: params[:comment]
